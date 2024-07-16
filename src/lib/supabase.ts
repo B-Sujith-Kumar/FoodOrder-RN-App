2a58b4b779299678138a8340147a184d83abcc9a
Banda Sujith Kumar <sujith.suave@gmail.com>
2024-07-16 11:49:00 +0530

    CHORE: Added supabase configuration.

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://vutlxkaslbyvpefyohue.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1dGx4a2FzbGJ5dnBlZnlvaHVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExMTA0MDMsImV4cCI6MjAzNjY4NjQwM30.-WV8C2l0jIHJm6WzALT_RCNlbmen5i0iMNByihCUE-A';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
